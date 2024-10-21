e9ca0aab7cc6442dffdd633c43f5ef4d8f22e7b2
Claudia Pando <claudiamfp@hotmail.com>
2024-10-21 17:34:25 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length <= 0 || pos > this.length){
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length <= 0){
+      throw new Error("Empty SortedList");
+    }
+    else {
+      return this.items.reduce((a, b) => Math.max(a, b), -Infinity)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length <= 0){
+      throw new Error("Empty SortedList");
+    }
+    else {
+      return this.items.reduce((a, b) => Math.min(a, b), +Infinity)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length <= 0) {
+      return 0;
+    }
+    else {
+      return this.items.reduce(function (acc, value) {
+        return acc + value
+      }, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    
+    if (this.length <= 0) {
+      throw new Error("Empty SortedList");
+    }
+    const result = this.items.reduce(function (acc, value) {
+    return acc + value
+    }, 0); 
+    return result / this.length;
+  }
 }
 
 module.exports = SortedList;
